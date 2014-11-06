584bcf7f772ace5dcc32c42ebbde43dce8722759
danReynolds <fnetma@hotmail.com>
2014-11-05 19:47:01 -0500

    update knowledge of certain things and consequently, the gem

--- lib/happy_path.rb
+++ lib/happy_path.rb
@@ -1,16 +1,15 @@
-require "happy_path/version"
-
 module HappyPath
   extend ActiveSupport::Concern
-
+  PATHS = ['first', 'last']
+  
   def setup_happy_path
     rescue_from ActiveRecord::RecordNotFound, with: :happy_paths
   end
 
-  module ClassMethods
-    def happy_paths(exception)
+  def follow_happy_paths
+    define_method (:happy_paths) do |exception|
       class_constant = controller_name.classify.constantize
-      raise exception unless ["first","last"].include?(params[:id]) || class_constant.count.zero?
+      raise exception unless self.respond_to?(:show) && HappyPath::PATHS.include?(params[:id]) && !class_constant.count.zero?
       redirect_to action: :show, id: class_constant.send(params[:id]).id
     end
   end
