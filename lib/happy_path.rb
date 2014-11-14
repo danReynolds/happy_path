5197f583d6610693454c646d6a9140aa53ae67d2
danReynolds <fnetma@hotmail.com>
2014-11-14 16:48:49 -0500

    only redirect on following paths, work with any action

--- lib/happy_path.rb
+++ lib/happy_path.rb
@@ -1,17 +1,17 @@
 module HappyPath
-  extend ActiveSupport::Concern
   PATHS = ['first', 'last']
-  CONTROLLERS = []
   
   def setup_happy_path
-    rescue_from ActiveRecord::RecordNotFound, with: :happy_paths if controller_name.classify.constantize
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      class_constant = controller_name.classify.constantize
+      raise e unless self.respond_to?(:happy_paths) && self.respond_to?(:show) && HappyPath::PATHS.include?(params[:id]) && !class_constant.count.zero?
+      self.happy_paths(e, class_constant)
+    end
   end
 
   def follow_happy_paths
-    define_method (:happy_paths) do |exception|
-      class_constant = controller_name.classify.constantize
-      raise exception unless self.respond_to?(:show) && HappyPath::PATHS.include?(params[:id]) && !class_constant.count.zero?
-      redirect_to action: :show, id: class_constant.send(params[:id]).id
+    define_method (:happy_paths) do |exception, class_constant|
+      redirect_to action: params[:action], id: class_constant.send(params[:id]).id
     end
   end
 end
