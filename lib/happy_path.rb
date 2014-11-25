bb5e97e2a4fa7ed5b93a01e72eb11d4c687a3a53
danReynolds <fnetma@hotmail.com>
2014-11-25 13:52:06 -0500

    split conditional to two lines

--- lib/happy_path.rb
+++ lib/happy_path.rb
@@ -3,7 +3,8 @@ module HappyPath
   
   def setup_happy_path
     rescue_from ActiveRecord::RecordNotFound do |e|
-      raise e unless controller_name.classify.constantize rescue nil && self.respond_to?(:happy_paths) && HappyPath::PATHS.include?(params[:id]) && !class_constant.count.zero?
+      class_constant = controller_name.classify.constantize rescue nil
+      raise e unless class_constant && self.respond_to?(:happy_paths) && HappyPath::PATHS.include?(params[:id]) && !class_constant.count.zero?
       self.happy_paths(e, class_constant)
     end
   end
