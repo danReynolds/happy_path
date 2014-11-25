5fbf796284a2e22689d513622572b680e06a3286
danReynolds <fnetma@hotmail.com>
2014-11-25 12:53:57 -0500

    rescue not try

--- lib/happy_path.rb
+++ lib/happy_path.rb
@@ -3,7 +3,7 @@ module HappyPath
   
   def setup_happy_path
     rescue_from ActiveRecord::RecordNotFound do |e|
-      raise e unless class_constant = controller_name.classify.try(:constantize) && self.respond_to?(:happy_paths) && HappyPath::PATHS.include?(params[:id]) && !class_constant.count.zero?
+      raise e unless controller_name.classify.constantize rescue nil && self.respond_to?(:happy_paths) && HappyPath::PATHS.include?(params[:id]) && !class_constant.count.zero?
       self.happy_paths(e, class_constant)
     end
   end
